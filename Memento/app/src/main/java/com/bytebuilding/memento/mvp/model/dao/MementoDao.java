4551acf1e3758c9486fc05d519d1975541cec331
alexeyturkin <altsworks@gmail.com>
2018-01-29 16:54:47 +0300

    * Added basic DAO structure for MementoDB

--- Memento/app/src/main/java/com/bytebuilding/memento/mvp/model/dao/MementoDao.java
+++ Memento/app/src/main/java/com/bytebuilding/memento/mvp/model/dao/MementoDao.java
@@ -1,6 +1,15 @@
 package com.bytebuilding.memento.mvp.model.dao;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.bytebuilding.memento.mvp.model.Memento;
+
+import java.util.List;
 
 /**
  * Created by Turkin A. on 29.01.2018.
@@ -8,4 +17,37 @@ import android.arch.persistence.room.Dao;
 
 @Dao
 public interface MementoDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Memento memento);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Memento... mementos);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(List<Memento> mementos);
+
+    @Update
+    void update(Memento memento);
+
+    @Update
+    void update(Memento... mementos);
+
+    @Update
+    void update(List<Memento> mementos);
+
+    @Delete
+    void delete(Memento memento);
+
+    @Delete
+    void delete(Memento... mementos);
+
+    @Delete
+    void delete(List<Memento> mementos);
+
+    //Queries
+
+    @Query("SELECT * FROM memento")
+    List<Memento> getAllMementos();
+
 }
